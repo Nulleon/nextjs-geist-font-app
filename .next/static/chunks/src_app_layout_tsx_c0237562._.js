51b8e8b2361c0b98cac11e4e1bd2c61e860f7432
Nulleon <nulleon777@gmail.com>
2025-05-27 22:26:06 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1748395565836

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__660e8f93._.js",
+  "static/chunks/[root-of-the-server]__8c5b3ecc._.css"
+],
+    source: "dynamic"
+});
